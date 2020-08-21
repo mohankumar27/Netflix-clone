da666d33bb3ba803a0fa103966950ae47415cc3e
mohankumar27 <mohankumar27197@gmail.com>
2020-08-21 12:31:49 +0530

    Updates

---
+++ precache-manifest.fdd86bb3cf1ca193b87edb79cb829c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0301100e44d14551b985d3e983791896",
+    "url": "/Netflix-clone/index.html"
+  },
+  {
+    "revision": "c639abb8087bfd344e0d",
+    "url": "/Netflix-clone/static/css/main.5578a3c9.chunk.css"
+  },
+  {
+    "revision": "e40f38824028de05bfb8",
+    "url": "/Netflix-clone/static/js/2.f84b350f.chunk.js"
+  },
+  {
+    "revision": "a435a3ec762d9159b1230d9b6e18884c",
+    "url": "/Netflix-clone/static/js/2.f84b350f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c639abb8087bfd344e0d",
+    "url": "/Netflix-clone/static/js/main.661b124e.chunk.js"
+  },
+  {
+    "revision": "2e6a87f45ab8bf4ca6d8",
+    "url": "/Netflix-clone/static/js/runtime-main.640d35e1.js"
+  }
+]);
